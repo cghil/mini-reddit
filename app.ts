254fca198b86533a0eda3c345fef2bd9363b87ef
Corey Ghilarducci <cghilarducci@gmail.com>
2016-08-11 21:56:00 -0700

    added directive for aritcles

--- app.ts
+++ app.ts
@@ -2,8 +2,10 @@
 import { bootstrap } from "@angular/platform-browser-dynamic";
 import { Component } from "@angular/core";
 
+// for RedditApp
 @Component({
 	selector: 'reddit',
+	directives: [ArticleComponent],
 	template:`
 		<form class="ui large form segment">
 			<h3 class="ui header">Add a Link</h3>
@@ -20,8 +22,71 @@ import { Component } from "@angular/core";
 				Submit Link
 			</button>
 		</form>
+		<div class="ui grid posts">
+			<reddit-article>
+			</reddit-article>
+		</div>
 	`
 })
+
+@Component({
+	selector: 'reddit-article',
+	host: {
+		class: 'row'
+	},
+	template: `
+		<div class="four wide column center aligned votes">
+			<div class="ui statistic">
+				<div class="value">
+					{{ votes }}
+				</div>
+				<div class="label">
+					Points
+				</div>
+			</div>
+		</div>
+		<div class="twelve wide column">
+			<a class="ui large header" href="{{ link }}">
+				{{title}}
+			</a>
+			<ul class="ui big horizontal list voters">
+				<li class="item">
+					<a href (click)="voteUp()">
+						<i class="arrow up icon">
+						</i>
+						upvote
+					</a>
+				</li>
+				<li class="item">
+					<a href (click)="voteDown()">
+						<i class="arrow down icon"></i>
+						downvote
+					</a>
+				</li>
+			</ul>
+		</div>
+	`
+})
+
+class ArticleComponent {
+	votes: number;
+	title: string;
+	link: string;
+
+	constructor(){
+		this.title = 'Angular 2';
+		this.link = 'http://angular.io';
+		this.votes = 10;
+	}
+
+	voteUp(){
+		this.votes += 1;
+	}
+
+	voteDown(){
+		this.votes-=1;
+	}
+}
 
 class RedditApp {
 	constructor(){
